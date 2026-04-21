7c79bce28a78f4d4af5e14ff5ba3dcfb27945df3
unknown <karindiaye3038@gmail.com>
2026-04-21 12:52:55 +0000

    premier commit du projet de Force

---
+++ script.js
@@ -0,0 +1,11 @@
+const menuBtn = document.getElementById('menufot');
+const sidebar = document.getElementById('sidebare');
+
+menuBtn.addEventListener('click', () => {
+  sidebar.classList.toggle('hidden');
+  
+});
+
+
+
+      
